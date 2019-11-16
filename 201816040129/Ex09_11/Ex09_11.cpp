9d9d1b85cbdedc002ef773e6c0563a5457a4ac40
201816040129 <56758137+201816040129@users.noreply.github.com>
2019-11-16 23:48:21 +0800

    Create Ex09_11.cpp
    This is my assignment_04
    Student ID:201816040129

---
+++ 201816040129/Ex09_11/Ex09_11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+#include "Rectangle.h"
+
+int main()
+{
+    cout<<"Please inter the length and width:"<<endl;
+    Rectangle rectangle;
+    rectangle.displayPerimeter();//output the perimeter
+    rectangle.displayArea();//output the area
+
+}
