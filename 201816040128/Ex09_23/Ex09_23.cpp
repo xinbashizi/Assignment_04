0f545d8d83ceb632475d0c685aadfccce09081b1
201816040128尚莹莹 <56549207+201816040128@users.noreply.github.com>
2019-11-17 15:15:27 +0800

    Create Ex09_23.cpp

---
+++ 201816040128/Ex09_23/Ex09_23.cpp
@@ -0,0 +1,12 @@
+#include"DeckOfCards.h"   
+#include"Card.h"   
+#include<ctime>   
+#include<iostream>   
+#include<vector>   
+using namespace std;
+
+int main()
+{
+	Card card(1, 2);
+	card.toString();
+}
