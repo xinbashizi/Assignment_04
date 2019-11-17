46b26cca93bc5ffecc1c000ac3c14d5b71b5efa5
201816040128尚莹莹 <56549207+201816040128@users.noreply.github.com>
2019-11-17 14:39:09 +0800

    Create Time.h

---
+++ 201816040128/Ex09_22/Time.h
@@ -0,0 +1,27 @@
+#ifndef TIME_H
+#define  TIME_H
+
+class Time
+{
+public:
+	explicit Time(int = 0, int = 0, int = 0);
+
+	void setTime(int, int, int);
+	void setHour(int);
+	void setMinute(int);
+	void setSecond(int);
+
+	int getHour() const;
+	int getMinute() const;
+	int getSecond() const;
+
+	void tick();
+	void printSecond() const;
+private:
+	int hour;
+	int minute;
+	int second;
+};
+#endif 
+
+
