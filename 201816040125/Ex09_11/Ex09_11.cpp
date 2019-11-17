6b1195d871ca97ea77c6bf90951f60ed65a3062c
201816040125 <56756064+201816040125@users.noreply.github.com>
2019-11-17 15:03:48 +0800

    Create Ex09_11.cpp
    This is my assignment_04
    Student ID:201816040125

---
+++ 201816040125/Ex09_11/Ex09_11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#include "Rectangle.h"
+
+int main()
+{  Rectangle r;
+  Rectangle Rectangle();
+  double a,b;
+
+ // cin>>a;
+  cout<<"lenght is ";
+  cin>>a;
+  cout<<endl;
+  cout<<"width is ";
+  cin>>b;
+  cout<<endl;
+  r.setRectangleNumber(a,b);
+   r. Area();
+    r.Perimeter();
+    return 0;
+} // end main
